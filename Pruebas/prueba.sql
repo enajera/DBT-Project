0149e299d74115c2505250877bf098fb18a02fb0
Elvin Nájera <elvin7n@gmail.com>
2023-01-18 00:12:32 +0000

    changes

--- Pruebas/prueba.sql
+++ Pruebas/prueba.sql
@@ -1,8 +1,4 @@
 -- Esto es una prueba GOBTPAC
 Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 
--- Ultima prueba
-Select * from DEV_RAW_DB.CANVAS.GOBTPAC; 
 
---Otra massss
- Select * from DEV_RAW_DB.CANVAS.GOBTPAC; 
