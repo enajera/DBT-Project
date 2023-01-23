5f003967afdb753142efb2a2024c02136496d508
Elvin Nájera <elvin7n@gmail.com>
2023-01-23 12:07:53 -0600

    lo quiero en main

--- Pruebas/prueba.sql
+++ Pruebas/prueba.sql
@@ -7,7 +7,7 @@ Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 -- mas de lo mismo  de antes
 Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 
--- Ahora estoy en nuevos cambios
+-- Ahora estoy en pero los quiero en main
 Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 
 -- Y voy a subir nuevos cambios
