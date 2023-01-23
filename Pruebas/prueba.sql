a6fe06d911242ef15137ff6da5a9014eaf1f1a5f
Elvin Nájera <elvin7n@gmail.com>
2023-01-23 09:23:46 -0600

    auto merge

--- Pruebas/prueba.sql
+++ Pruebas/prueba.sql
@@ -3,5 +3,7 @@ Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 
 -- Otras pruebas vbvb
 Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
+
 -- mas de lo mismo  de antes
+Select * from DEV_RAW_DB.CANVAS.GOBTPAC;
 
